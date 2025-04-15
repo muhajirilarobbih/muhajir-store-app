19d173344ab56008e4009741129b27e50d3534ed
muhajirradyalabs <muhajir@radyalabs.com>
2025-04-16 00:56:28 +0700

    implement my aplication

---
+++ app/src/main/java/com/example/muhajirstoreapp/view/MyApplication.kt
@@ -0,0 +1,6 @@
+package com.example.muhajirstoreapp.view// com.example.muhajirstoreapp.view.MyApplication.kt
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
